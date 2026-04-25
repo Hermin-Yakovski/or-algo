bebd3044d28011ab4f95f7058cd961411f241ef9
yehemin <hemin.ye@qq.com>
2026-04-25 11:31:12 +0800

    feat: add OrAlgoException base class

---
+++ or_algo/exception.py
@@ -0,0 +1,6 @@
+"""Exception classes for or-algo package."""
+
+
+class OrAlgoException(Exception):
+    """Base exception for all or-algo package errors."""
+    pass
---
+++ tests/test_exception.py
@@ -0,0 +1,29 @@
+"""Tests for or_algo.exception module."""
+
+import pytest
+from or_algo.exception import OrAlgoException
+
+
+def test_or_algo_exception_creation():
+    """Test that OrAlgoException can be created with a message."""
+    exc = OrAlgoException("test error")
+    assert str(exc) == "test error"
+    assert isinstance(exc, Exception)
+
+
+def test_or_algo_exception_as_base_class():
+    """Test that OrAlgoException can be used as a base class."""
+    class CustomError(OrAlgoException):
+        pass
+
+    exc = CustomError("custom message")
+    assert isinstance(exc, OrAlgoException)
+    assert isinstance(exc, Exception)
+    assert str(exc) == "custom message"
+
+
+def test_or_algo_exception_without_message():
+    """Test that OrAlgoException can be created without a message."""
+    exc = OrAlgoException()
+    assert isinstance(exc, Exception)
+    assert isinstance(exc, OrAlgoException)
