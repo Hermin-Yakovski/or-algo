f2d38cfa33f8c0f78bc55e58ee93516376711ef2
yehemin <hemin.ye@qq.com>
2026-05-09 16:22:01 +0800

    refactor: remove SharedRegister class and tests

--- or_algo/__init__.py
+++ or_algo/__init__.py
@@ -4,7 +4,6 @@ from .solver import Solver
 from .algorithm import Algorithm
 from .exception import OrAlgoException
 from .task import SolverTask
-from .shared_register import SharedRegister
 
 __version__ = "0.2.0"
 
@@ -14,7 +13,6 @@ __all__ = [
     "OrAlgoException",
     "lp",
     "SolverTask",
-    "SharedRegister",
 ]
 
 
